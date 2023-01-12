0aa3ef81aa257940e1a8e72c4a69bc7d39787510
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2023-01-12 21:44:16 +0400

    fix transactions history overlay

--- components/transactionQueue/transactionQueue.js
+++ components/transactionQueue/transactionQueue.js
@@ -178,7 +178,7 @@ export default function TransactionQueue({setQueueLength}) {
       maxWidth={"sm"}
       TransitionComponent={Transition}
       fullScreen={fullScreen}
-      BackdropProps={{style: {backgroundColor: 'transparent'}}}
+      // BackdropProps={{style: {backgroundColor: 'transparent'}}}
     >
       <div className={classes.tvAntenna}>
         <svg width="40" height="28" viewBox="0 0 40 28" fill="none" xmlns="http://www.w3.org/2000/svg">
